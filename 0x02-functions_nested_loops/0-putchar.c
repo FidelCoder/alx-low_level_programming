8d27ea3081bb761705666dcba5ec7537232b6737
Griffins-sys254 <griffinesonyango@gmail.com>
2022-03-15 20:43:54 +0300

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0 Always
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
