6be03fa374cf48c609322ededfba35ed8b596387
Griffins-sys254 <griffinesonyango@gmail.com>
2022-03-21 23:45:20 +0300

    0-reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  print squares
+ * @n : defines number of  # and ' '
+ * Return: void
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
