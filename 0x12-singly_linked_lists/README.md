b70ada969c27eceba8299468b692d3c0bf2ff0cd
Griffins-sys254 <griffinesonyango@gmail.com>
2022-12-18 12:42:37 +0300

    added project files

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Data structures: Singly linked lists
---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Function to compute the perimeter of an island """
+
+
+def island_perimeter(grid):
+    """ Return perimeter of the island """
+    perim = 0
+    for y, row in enumerate(grid):
+        for x, item in enumerate(row):
+            if item == 1:
+                if x == 0 or grid[y][x - 1] == 0:
+                    perim += 1
+                if x == len(row) - 1 or grid[y][x + 1] == 0:
+                    perim += 1
+                if y == 0 or grid[y - 1][x] == 0:
+                    perim += 1
+                if y == len(grid) - 1 or grid[y + 1][x] == 0:
+                    perim += 1
+    return perim
